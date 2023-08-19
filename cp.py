a84c0f9af402f0c148d4478871ff6a5a6bf741b8
hap2919 <142662437+hap2919@users.noreply.github.com>
2023-08-19 15:30:07 +0530

    Add files via upload

---
+++ cp.py
@@ -0,0 +1,11 @@
+import time
+
+i = input('Enter Id : ')
+x = input('Enter password : ')
+time.sleep(3)
+if i=="9450105282" and x=="i813@kl":
+	print("Package is installing....")
+time.sleep(20)
+for ii in range (2122118):
+	print(ii)
+print("Your device is not supported*")
